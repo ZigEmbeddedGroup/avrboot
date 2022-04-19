dff8581492c2a05c59718e030c45454aafb8af8d
Auguste Rame <auguste.rame@gmail.com>
2022-04-19 19:28:42 -0400

    Syncing with Arduino Uno works!

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,48 @@ const std = @import("std");
 const testing = std.testing;
 
 const serial = @import("serial.zig");
-pub const stk = @import("stk500v2/stk.zig");
+pub const stk = @import("stk500/stk.zig");
 
-pub fn touchSerialPortAt1200Bps(port: []const u8) !void {
+const CommTimeouts = extern struct {
+    read_interval_timeout: std.os.windows.DWORD,
+    read_total_timeout_multiplier: std.os.windows.DWORD,
+    read_total_timeout_constant: std.os.windows.DWORD,
+    write_total_timeout_multiplier: std.os.windows.DWORD,
+    write_total_timeout_constant: std.os.windows.DWORD,
+};
+
+extern "kernel32" fn GetCommTimeouts(
+    hFile: std.os.windows.HANDLE,
+    lpCommTimeouts: *CommTimeouts,
+) std.os.windows.BOOL;
+
+extern "kernel32" fn SetCommTimeouts(
+    hFile: std.os.windows.HANDLE,
+    lpCommTimeouts: *CommTimeouts,
+) std.os.windows.BOOL;
+
+fn reset(port: std.fs.File) !void {
+    // Fun fact: avrdude does
+    // RTS/DTR = 0
+    // ...
+    // RTS/DTR = 1
+    // ...
+    // but this didn't work on my machine (which was usb-to-serial; might have to do with the behavior?)
+    // so I flipped the pattern and it worked!
+
+    try serial.changeControlPins(port, .{ .rts = true, .dtr = true });
+    std.time.sleep(250 * std.time.ns_per_ms);
+
+    try serial.changeControlPins(port, .{ .rts = false, .dtr = false });
+    std.time.sleep(50 * std.time.ns_per_ms);
+}
+
+pub fn main() !void {
+    var it = try serial.list();
+    while (try it.next()) |port| {
+        std.debug.print("{s} (file: {s}, driver: {s})\n", .{ port.display_name, port.file_name, port.driver });
+    }
+
     var cfg = serial.SerialConfig{
         .handshake = .none,
         .baud_rate = 115200,
@@ -13,72 +52,54 @@ pub fn touchSerialPortAt1200Bps(port: []const u8) !void {
         .stop_bits = .one,
     };
 
-    var port_file = try std.fs.cwd().openFile(
-        port, // if any, these will likely exist on a machine
+    var port = try std.fs.cwd().openFile(
+        "\\\\.\\COM3", // if any, these will likely exist on a machine
         .{ .mode = .read_write },
     );
+    defer port.close();
 
-    try serial.configureSerialPort(port_file, cfg);
+    var ct = std.mem.zeroes(CommTimeouts);
+    if (GetCommTimeouts(port.handle, &ct) == 0)
+        return error.WindowsError;
 
-    try serial.changeControlPins(port_file, .{ .rts = false, .dtr = false });
-    std.time.sleep(250 * std.time.ns_per_ms);
+    ct.read_interval_timeout = 0;
+    ct.read_total_timeout_multiplier = 100;
+    ct.read_total_timeout_constant = 100;
 
-    try serial.changeControlPins(port_file, .{ .rts = true, .dtr = true });
-    std.time.sleep(50 * std.time.ns_per_ms);
+    if (SetCommTimeouts(port.handle, &ct) == 0)
+        return error.WindowsError;
 
-    try serial.flushSerialPort(port_file, true, true);
+    try serial.configureSerialPort(port, cfg);
 
-    var reader = port_file.reader();
-    // var writer = port_file.writer();
+    var reader = port.reader();
+    var writer = port.writer();
 
-    // var init_msg = stk.Message{
-    //     .sequence_number = 0,
-    //     .body = .{ .command = .sign_on },
-    // };
+    try reset(port);
+    try serial.flushSerialPort(port, true, true);
 
-    // try init_msg.encode(writer);
+    const buf = &[_]u8{ @enumToInt(stk.CommandId.get_sync), 0x20 };
 
-    while (true) {
-        var buf: [10]u8 = undefined;
-        _ = try reader.readAll(&buf);
-        std.log.err("{d}", .{buf});
+    var attempt: usize = 0;
+    while (attempt < 32) : (attempt += 1) {
+        std.log.info("Sync attempt {d}...", .{attempt});
+
+        if (attempt > 0) {
+            try reset(port);
+            try serial.flushSerialPort(port, true, true);
+        }
+
+        try serial.flushSerialPort(port, true, true);
+        try writer.writeAll(buf);
+
+        if ((reader.readByte() catch continue) == @enumToInt(stk.ResponseStatus.in_sync)) {
+            std.log.info("In sync!", .{});
+            break;
+        } else {
+            @panic("so this shouldnt happen...");
+        }
     }
 
-    port_file.close();
-}
-
-pub fn main() !void {
-    var it = try serial.list();
-    while (try it.next()) |port| {
-        std.debug.print("{s} (file: {s}, driver: {s})\n", .{ port.display_name, port.file_name, port.driver });
-    }
-    try touchSerialPortAt1200Bps(if (@import("builtin").os.tag == .windows) "\\\\.\\COM3" else "/dev/ttyUSB0");
-    // it = try serial.list();
-    // while (try it.next()) |port| {
-    //     std.debug.print("{s} (file: {s}, driver: {s})\n", .{ port.display_name, port.file_name, port.driver });
-    // }
-
-    // var reader = port.reader();
-    // var writer = port.writer();
-
-    // try serial.changeControlPins(port, .{ .dtr = false });
-
-    // var init_msg = stk.Message{
-    //     .sequence_number = 0,
-    //     .body = .{ .command = .sign_on },
-    // };
-
-    // try init_msg.encode(writer);
-
-    // while (true) {
-    //     var buf: [10]u8 = undefined;
-    //     _ = try port.reader().readAll(&buf);
-    //     std.log.err("{d}", .{buf});
-    // }
-
-    // std.log.err("{s}", .{
-    //     try stk.Message.decode(reader, .answer),
-    // });
+    // writer.writeAll(&[_]u8{});
 }
 
 test {
--- src/stk500/stk.zig
+++ src/stk500/stk.zig
@@ -39,16 +39,72 @@ pub const CommandId = enum(u8) {
     read_osccal_ext = 0x78,
 };
 
+pub const CommandError = error{
+    Failed,
+    Unknown,
+    ADCChannel,
+    PWMChannel,
+};
+
 pub const ResponseStatus = enum(u8) {
-    stk_ok = 0x10,
-    stk_failed = 0x11,
-    stk_unknown = 0x12,
-    stk_nodevice = 0x13,
-    stk_insync = 0x14,
-    stk_nosync = 0x15,
+    ok = 0x10,
+    failed = 0x11,
+    unknown = 0x12,
+    no_device = 0x13,
+    in_sync = 0x14,
+    no_sync = 0x15,
 
     adc_channel_error = 0x16,
     adc_measure_ok = 0x17,
     pwm_channel_error = 0x18,
     pwm_adjust_ok = 0x19,
+};
+
+pub const Parameter = enum(u8) {
+    const RWArray = std.enums.EnumArray(Parameter, struct { read: bool, write: bool });
+    pub const RW: RWArray = RWArray.init(.{
+        .hw_ver = .{ .read = true, .write = false },
+        .sw_major = .{ .read = true, .write = false },
+        .sw_minor = .{ .read = true, .write = false },
+        .leds = .{ .read = true, .write = true },
+        .vtarget = .{ .read = true, .write = true },
+        .vadjust = .{ .read = true, .write = true },
+        .osc_pscale = .{ .read = true, .write = true },
+        .osc_cmatch = .{ .read = true, .write = true },
+        .reset_duration = .{ .read = true, .write = true },
+        .sck_duration = .{ .read = true, .write = true },
+
+        .bufsizel = .{ .read = true, .write = false },
+        .bufsizeh = .{ .read = true, .write = false },
+        .device = .{ .read = true, .write = false },
+        .progmode = .{ .read = true, .write = false },
+        .paramode = .{ .read = true, .write = false },
+        .polling = .{ .read = true, .write = false },
+        .selftimed = .{ .read = true, .write = false },
+        .topcard_detect = .{ .read = true, .write = false },
+    });
+
+    hw_ver = 0x80,
+    sw_major = 0x81,
+    sw_minor = 0x82,
+    leds = 0x83,
+    vtarget = 0x84,
+    vadjust = 0x85,
+    osc_pscale = 0x86,
+    osc_cmatch = 0x87,
+    reset_duration = 0x88,
+    sck_duration = 0x89,
+
+    bufsizel = 0x90,
+    bufsizeh = 0x91,
+    device = 0x92,
+    /// 'P' or 'S'
+    progmode = 0x93,
+    /// "TRUE" or "FALSE"
+    paramode = 0x94,
+    /// "TRUE" or "FALSE"
+    polling = 0x95,
+    /// "TRUE" or "FALSE"
+    selftimed = 0x96,
+    topcard_detect = 0x98,
 };
